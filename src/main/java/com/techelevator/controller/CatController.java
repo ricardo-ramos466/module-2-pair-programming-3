99204afd4c53c8a8e48b24cdffda6d38fc9275ac
Ricardo Ramos <ricardo.r.p466@gmail.com>
2022-08-10 22:36:07 -0400

    done

--- src/main/java/com/techelevator/controller/CatController.java
+++ src/main/java/com/techelevator/controller/CatController.java
@@ -6,52 +6,57 @@ import com.techelevator.services.CatFactService;
 import com.techelevator.services.CatPicService;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api")
+@RequestMapping(path = "/api/cards")
 public class CatController {
 
     private CatCardDao catCardDao;
     private CatFactService catFactService;
     private CatPicService catPicService;
+    private CatCard card = new CatCard();
 
     public CatController(CatCardDao catCardDao, CatFactService catFactService, CatPicService catPicService) {
         this.catCardDao = catCardDao;
         this.catFactService = catFactService;
         this.catPicService = catPicService;
     }
-    @GetMapping(path = "/cards")
+    @GetMapping()
     public List<CatCard> list(){
         return catCardDao.list();
     }
 
-    @GetMapping(path = "/cards/{id}")
+    @GetMapping(path = "/{id}")
     public CatCard get(@PathVariable int id){
         return catCardDao.get(id);
     }
-    @GetMapping(path = "/cards/random")
+    @GetMapping(path = "/random")
     public CatCard random(){
-        int cardId = 0;
-        while(cardId==0 || cardId>catCardDao.list().size()) {
-            cardId = (int) (Math.random() * 10);
-        }
-        return catCardDao.get(cardId);
+//        int cardId = 0;
+//        while(cardId==0 || cardId>catCardDao.list().size()) {
+//            cardId = (int) (Math.random() * 10);
+//        }
+//        return catCardDao.get(cardId);
+        card.setCatFact(catFactService.getFact().getText());
+        card.setImgUrl(catPicService.getPic().getFile());
+        return card;
     }
 
-    @PostMapping(path = "/cards")
-    public CatCard save(CatCard card){
+    @PostMapping()
+    public CatCard save(@Valid @RequestBody CatCard card){
         catCardDao.save(card);
         return card;
     }
 
-    @PutMapping(path = "/cards/{id}")
-    public CatCard update(@PathVariable int id, CatCard card){
+    @PutMapping(path = "/{id}")
+    public CatCard update(@PathVariable int id, @Valid @RequestBody CatCard card){
         catCardDao.update(id,card);
         return card;
     }
 
-    @DeleteMapping(path = "/cards/{id}")
+    @DeleteMapping(path = "/{id}")
     public void delete(@PathVariable int id){
         catCardDao.delete(id);
     }
